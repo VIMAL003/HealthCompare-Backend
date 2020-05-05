6d8573e67ec101592c88cfe87b1db155403cdd60
Derick Renwick <derick_renwick@hotmail.com>
2020-05-05 01:05:17 -0400

    Home Hero setup

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -12,101 +12,17 @@
 
 @section('content')
 
-    @include('pages.pagesections.banner')
+    @include('ui.section.home_hero')
 
-    <section class="how-section__parent">
-        @include('components.howsection', [
-            'section'           => true,
-            'hideButton'        => false,
-            'sectionTitle'      => 'How to Use Hospital Compare',
-            'howsections'       => [
-            [
-                'iconImg'       => 'search-and-compare',
-                'step'          => 'One',
-                'title'         => 'Search & compare:',
-                'color'         => 'violet',
-                'description'   => '<p>Search from over 750 hospitals in England and find the right hospital for your treatment.</p>'],
-            [
-                'iconImg'       => 'make-enquiry',
-                'step'          => 'Two',
-                'title'         => 'Make enquiry:',
-                'color'         => 'pink',
-                'description'   => '<p>Use Hospital Compare to make an enquiry at one or more hospitals, without obligation.</p>'],
-            [
-                'iconImg'       => 'request-a-referral',
-                'step'          => 'Three',
-                'title'         => 'Book Appointment:',
-                'color'         => 'blue',
-                'description'   => '<p>For self-pay or insurance, book your appointment directly. For NHS treatment, make enquiries then inform your GP of your choice to book an appointment (you will need a GP referral).</p>'
-            ]
-        ]
-    ])
-    </section>
 
-    <section class="why-use-parent">
-        <div class="why-use-content">
-            <div class="why-use container">
-                <div class="row">
-                    <div class="why-use-text col col-12 col-md-6">
-                        <h2 class="section-title">Why use Hospital Compare?</h2>
-                        <p class="p-intro">Hospital Compare helps you make the right choice when looking for the best hospital for your treatment.
-                        </p>
-                        <p class="col-grey">Many people in England aren’t aware they have a choice as to where to have their NHS-funded treatment. This choice includes both NHS and private hospitals. We’re here to help you understand your rights and make the best decision.</p>
-                        <p class="col-grey">Whether you’re searching for the shortest waiting times or a hospital with the highest quality ratings, Hospital Compare provides free, unbiased, accurate and up-to-date information on over 750 NHS and private hospitals across England.</p>
-                    </div>
-                    <div class="why-use-video col col-12 col-md-6">
-                        <img class="w-100" src="{{ url('/images/home-1.jpg') }}" alt="Doctor and patient in consultation">
-{{--                        <div class="video-wrapper">--}}
-{{--                            <video muted class="content" poster="{{ url('images/home-1.jpg') }}">--}}
-{{--                                <source src="{{ asset('video/For_Wes.mp4') }}" type="video/mp4">--}}
-{{--                                <source src="movie.ogg" type="video/ogg">--}}
-{{--                                Your browser does not support the video tag.--}}
-{{--                            </video>--}}
-                            {{--                            <div class="player-button toggle"></div>--}}
-{{--                        </div>--}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-{{--    @include('pages.pagesections.testimonials')--}}
-    @if(!empty($data['faqs']))
-        <section class="faqs-section bg-grey">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <h2 class="section-title">Frequently asked questions</h2>
-                        <p class="col-grey lh-16 font-18">Still have a few things you'd like to clear up? We've put together the
-                            following simple answers to questions frequently thought, but rarely asked, about finding
-                            the right hospital for your treament.</p>
-                        <div class="hc-faqs-accordion pt-3" id="_faqs_accordion">
-                            @foreach($data['faqs'] as $key => $faq)
-                                <div class="card p-30">
-                                    <div class="card-header mb-3 rounded-0 p-0 bg-white border-0" id="heading{{$key}}">
-                                        <p class="mb-0 font-16 SofiaPro-Medium">
-                                            {!! $faq->question !!}
-                                        </p>
-                                    </div>
+    {{-- @include('ui.section.compare_nav_blocks') --}}
 
-                                    <div id="collapse{{$key}}" class="collapse" aria-labelledby="heading{{$key}}"
-                                         data-parent="#faqs_accordion">
-                                        <div class="card-body pr-0 pb-0">
-                                            {!! $faq->answer !!}
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>{{-- Accordion --}}
-                        <div class="btn-area text-center text-lg-left">
-                            @include('components.basic.button', [
-                                'buttonText'    => 'View all FAQs',
-                                'classTitle'    => 'btn btn-brand-primary-1 btn-squared mt-4 font-18',
-                                'hrefValue'     => '/faqs'
-                            ])
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    @endif
+    {{-- @include('ui.section.header_2_columns', ['wrapperClass'=>"pb-lg-5"]) --}}
+
+    {{-- @include('ui.section.cta_banner_overflowing_image') --}}
+
+    {{-- @include('ui.section.compare_online_cards') --}}
+
+    {{-- @include('ui.section.faq') --}}
+
 @endsection
---
+++ resources/views/ui/section/home_hero.blade.php
@@ -0,0 +1,66 @@
+<div class="container_fluid home-hero pt-3">
+  <div class="container">
+    <div class="row">
+
+      @hasSection('main-content')
+        @yield('main-content')
+      @else
+        <div class="col-12 col-lg-6 main-content d-lg-none order-1 order-lg-1">
+          <h4>{!!  $homeHero['slogan'] !!}</h4>
+          {{-- <img src="../images/your-health-your-choice-footer-blue.svg" width="200" alt=""> --}}
+          <h2>{!! $homeHero['heading'] !!}</h2>
+          <p>{{$homeHero['subtitle']}}</p>
+        </div>
+      @endif
+
+
+      @hasSection('home-search')
+        @yield('home-search')
+      @else
+        <div class="col-12 col-lg-12 home-search order-2 order-lg-3">
+          {{-- @include('ui.component.search_form') --}}
+        </div>
+      @endif
+
+      @hasSection('did-you-know')
+        @yield('did-you-know')
+      @else
+      <div class="col-12 col-lg-6 did-you-know d-lg-none order-3 order-lg-2">
+        <h3 class="mt-2">Did you know:</h3>
+        @foreach( $homeHero['didYouKnowList'] as $item)
+          <img src="../images/tick.svg" alt="" style="float:left;">
+          <p>{{$item}}</p>
+        @endforeach
+      </div>
+      @endif
+
+      @hasSection('desktop-main-content')
+        @yield('desktop-main-content')
+      @else
+      <div class="col-6 desktop-main-content d-none d-lg-block order-lg-1">
+        <div class="main-content pt-5">
+          {{-- <h4>{{{$homeHero['slogan']}}}</h4> --}}
+          <h2>{!! $homeHero['heading'] !!}</h2>
+          <p>{{$homeHero['subtitle']}}</p>
+        </div>
+        <div class="did-you-know py-3">
+          <h3>Did you know:</h3>
+          @foreach( $homeHero['didYouKnowList'] as $item)
+          <img src="../images/tick.svg" alt="" style="float:left;">
+          <p>{{$item}}</p>
+        @endforeach
+        </div>
+      </div>
+      @endif
+
+      @hasSection('desktop-secondary-content')
+        @yield('desktop-secondary-content')
+      @else
+      <div class="col-12 col-lg-6 desktop-secondary-content order-4 d-none d-lg-block order-lg-2">
+        <img src="../images/rcd/your-health-your-choice.svg" alt="" class="white-circular-logo">
+        <img class="img-fluid" src="../images/rcd/runners.png" alt="">
+      </div>
+      @endif
+    </div>
+  </div>
+</div>
