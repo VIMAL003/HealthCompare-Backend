c48b93fdfae342ad5d93af781b3647eae18ba5f0
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 17:17:08 -0400

    Changed from font-awesome to svg (WIP)

--- resources/views/ui/component/rating_input.blade.php
+++ resources/views/ui/component/rating_input.blade.php
@@ -1,18 +1,38 @@
 <ul class="rating">
   <li class="star filled">
-    <i class="fas fa-star"></i>
+    @svg('star-full', 'star-full')
+    @svg('star-green', 'star-green')
+    @svg('star-half', 'star-half')
+    @svg('star-outline', 'star-outline')
+    {{-- <i class="fas fa-star"></i> --}}
   </li>
   <li class="star filled">
-    <i class="fas fa-star"></i>
+    @svg('star-full', 'star-full')
+    @svg('star-green', 'star-green')
+    @svg('star-half', 'star-half')
+    @svg('star-outline', 'star-outline')
+    {{-- <i class="fas fa-star"></i> --}}
   </li>
   <li class="star filled">
-    <i class="fas fa-star"></i>
+    @svg('star-full', 'star-full')
+    @svg('star-green', 'star-green')
+    @svg('star-half', 'star-half')
+    @svg('star-outline', 'star-outline')
+    {{-- <i class="fas fa-star"></i> --}}
   </li>
   <li class="star">
-    <i class="fas fa-star"></i>
+    @svg('star-full', 'star-full')
+    @svg('star-green', 'star-green')
+    @svg('star-half', 'star-half')
+    @svg('star-outline', 'star-outline')
+    {{-- <i class="fas fa-star"></i> --}}
   </li>
   <li class="star">
-    <i class="fas fa-star"></i>
+    @svg('star-full', 'star-full')
+    @svg('star-green', 'star-green')
+    @svg('star-half', 'star-half')
+    @svg('star-outline', 'star-outline')
+    {{-- <i class="fas fa-star"></i> --}}
   </li>
 </ul>
 <input type="hidden" name="rating" value="3">
