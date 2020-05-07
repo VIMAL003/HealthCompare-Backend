c926a437b045547a205f42a1f80da6996d7a52ce
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 14:54:38 -0400

    Update HTML Title prefix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,7 +11,7 @@
             <!-- End Google Tag Manager -->
         @endif
 
-        <title>Hospital Compare - @yield('title')</title>
+        <title>Health Compare - @yield('title')</title>
         <meta charset='utf-8'>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name='description' content='@yield('description')'>
