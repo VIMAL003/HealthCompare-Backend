6cde3181119a67ac651e4ddf8ba2a85396106215
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 12:32:06 -0400

    Remove compare title

--- resources/views/ui/section/compare_nav_blocks.blade.php
+++ resources/views/ui/section/compare_nav_blocks.blade.php
@@ -1,6 +1,6 @@
 <div class="container_fluid compare-nav-blocks py-3">
   <div class="container px-2 pb-5">
-    <h3>{{$compareNavBlocks['heading']}}</h3>
+    {{-- <h3>{{$compareNavBlocks['heading']}}</h3> --}}
     <div class="row row-cols-3 row-cols-lg-4 mx-n2">
       {{-- <div class="card-deck"> --}}
       @foreach ( $compareNavBlocks['cards'] as $card)
