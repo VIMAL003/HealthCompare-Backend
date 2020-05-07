559e115d5b445f6633a16ab79f1f042533a252f3
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 16:24:41 -0400

    cookie link

--- resources/views/ui/component/cookie_nag.blade.php
+++ resources/views/ui/component/cookie_nag.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
       <div class="alert alert-dismissible fade show rounded-0 mb-0 py-3 px-0" role="alert">
         <p class="mb-0 text-white">We use cookies to provide a better service. By closing this window or continuing to use
-          our website you agree with our use of cookies. Find out how to manage cookies and view our policy <a href="#policy">here</a>.</p>
+          our website you agree with our use of cookies. Find out how to manage cookies and view our policy <a href="{{ url('/cookie-policy') }}">here</a>.</p>
         <button type="button" class="close btn bg-brandColor2 text-white mt-3 py-2 px-2 font-weight-normal" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">Close</span>
         </button>
