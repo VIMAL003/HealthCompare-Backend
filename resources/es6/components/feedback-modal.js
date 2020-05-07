b219fc8e4e158de4f224d474f032f8317c3d2bbc
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 17:24:29 -0400

    Temporarily disable feedback modal

--- resources/es6/components/feedback-modal.js
+++ resources/es6/components/feedback-modal.js
@@ -6,7 +6,7 @@ import 'bootstrap/js/dist/modal';
 //     show:true,
 //     backdrop:'static'
 // });
-if ( document.querySelector('#feedbackModal') ){
+if ( document.querySelector('#feedbackModal') && false ){
 
   let blurred = false;
   window.addEventListener('focus', (e) => {
