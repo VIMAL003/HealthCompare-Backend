c75c178f6d2518994d052393178eb98eaef25cd2
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 19:31:05 -0400

    Added target for navblocks

--- resources/views/ui/component/landing_nav_blocks.blade.php
+++ resources/views/ui/component/landing_nav_blocks.blade.php
@@ -1,7 +1,7 @@
 <div class="row row-cols-1 row-cols-lg-3 mx-n2 white-text">
   @foreach ($cards as $card )
   <div class="col mb-4 px-2 {{$card['cardClass']}}">
-    <a href="{{$card['permalink']}}" class="card h-100 mb-lg-3 p-3">
+    <a href="{{url($card['permalink'])}}" target="{{ $card['cardClass'] == 'open-in-new-tab' ? '_blank' : ''}}" @if($card['cardClass'] == "open-in-new-tab") target="_blank" @endif class="card h-100 mb-lg-3 p-3">
       <img src="{{ \App\Helpers\Utils::convertAssetBaseUrl($card['imgUrl'])}}" class="mx-auto" alt="">
       <div class="card-body p-0">
         <h5 class="text-brandColor1">{{$card['heading']}}</h5>
