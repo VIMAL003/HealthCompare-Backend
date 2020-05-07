615ac05a1171d4e580b7f4b818e70b81c3bacdc0
Derick Renwick <derick_renwick@hotmail.com>
2020-05-07 12:31:30 -0400

    updated nhs logo url

--- resources/views/ui/component/notice_nag.blade.php
+++ resources/views/ui/component/notice_nag.blade.php
@@ -3,7 +3,7 @@
       <div class="alert alert-dismissible text-white fade show rounded-0 mb-0 py-4 px-0" role="alert">
         <p class="mb-0">
           <a class="text-white mr-3" style="text-decoration: underline;" href="#">Health Compare COVID-19 position</a>
-          Supporting our <img class="nhs-logo" src="http://hcstaging.co.uk/hospital-compare/public/images/icons/nhs-logo.svg" height="20" alt="">
+          Supporting our <img class="nhs-logo" src="{{ secure_asset('images/rcd/nhs-logo.svg') }}" height="20" alt="">
         </p>
         <button type="button" class="close text-white" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
